4373ad3343f46656d0a6e3f0e3f625745cddcb27
JTamashi <firsth256@gmail.com>
2026-05-13 16:31:29 +0500

    done

--- Program.cs
+++ Program.cs
@@ -1,78 +1,145 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace azizov_abdulaziz_bilet_21
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-
-        }
-    }
-
     class AdCampaign
     {
-        // Audience
-        public int demographics { set; get; }
-        public string location { set; get; }
+        public string Audience;
+        public double Budget;
+        public List<string> Channels = new List<string>();
+        public string Schedule;
+        public string TargetingOptions;
+        public List<string> Metrics = new List<string>();
 
-        // Budget
-        public int amount {  set; get; }
-        public DateTime duration { set; get; }
-
-        // Channels
-        public string channel { set; get; }
-
-        // Schedule
-        public int start { set; get; }
-        public int end { set; get; }
-
-        // TargetingOption
-        public string options { set; get; }
-
-        // Metrics
-        public string metrics { set; get; }
+        public void ShowInfo()
+        {
+            Console.WriteLine("Аудитория: " + Audience);
+            Console.WriteLine("Бюджет: " + Budget + "$");
+            Console.WriteLine("Каналы: " + string.Join(", ", Channels));
+            Console.WriteLine("Расписание: " + Schedule);
+            Console.WriteLine("Таргетинг: " + TargetingOptions);
+            Console.WriteLine("Метрики: " + string.Join(", ", Metrics));
+            Console.WriteLine();
+        }
     }
 
-    class Campaigh : AdCampaign
+    class Campaign
     {
-        public void SetAudience(int demographics, string location)
+        private AdCampaign campaign = new AdCampaign();
+
+        public void SetAudience(string demographics, string location)
         {
-            this.demographics = demographics;
-            this.location = location;
+            campaign.Audience = demographics + ", " + location;
         }
 
-        public void SetBudget(int amount, DateTime duration)
+        public void SetBudget(double amount, string duration)
         {
-            this.amount = amount;
-            this.duration = duration;
+            campaign.Budget = amount;
         }
 
         public void AddChannel(string channel)
         {
-            this.channel = channel;
+            campaign.Channels.Add(channel);
         }
 
-        public void SetSchedule(int start,  int end)
+        public void SetSchedule(string start, string end)
         {
-            this.start = start;
-            this.end = end;
+            campaign.Schedule = "с " + start + " по " + end;
         }
 
         public void SetTargetingOptions(string options)
         {
-            this.options = options;
+            campaign.TargetingOptions = options;
         }
 
-        public void DefineMetric(string metrics)
+        public void DefineMetrics(string metrics)
         {
-            this.metrics = metrics;
+            campaign.Metrics.Add(metrics);
+        }
+
+        public AdCampaign Build()
+        {
+            AdCampaign result = campaign;
+            campaign = new AdCampaign();
+            return result;
         }
     }
 
+    class SocialMediaCampaign
+    {
+        public static AdCampaign Create()
+        {
+            Campaign builder = new Campaign();
+            builder.SetAudience("18-25 лет", "Алматы");
+            builder.SetBudget(1000, "30 дней");
+            builder.AddChannel("Instagram");
+            builder.AddChannel("TikTok");
+            builder.SetSchedule("01.06.2026", "30.06.2026");
+            builder.SetTargetingOptions("По интересам");
+            builder.DefineMetrics("Клики");
+            builder.DefineMetrics("Показы");
+            return builder.Build();
+        }
+    }
 
+    class SearchEngineCampaign
+    {
+        public static AdCampaign Create()
+        {
+            Campaign builder = new Campaign();
+            builder.SetAudience("Все возрасты", "Казахстан");
+            builder.SetBudget(2000, "30 дней");
+            builder.AddChannel("Google");
+            builder.AddChannel("Yandex");
+            builder.SetSchedule("01.06.2026", "30.06.2026");
+            builder.SetTargetingOptions("По ключевым словам");
+            builder.DefineMetrics("Клики");
+            builder.DefineMetrics("Просмотры");
+            return builder.Build();
+        }
+    }
+
+    class EmailMarketingCampaign
+    {
+        public static AdCampaign Create()
+        {
+            Campaign builder = new Campaign();
+            builder.SetAudience("Постоянные клиенты", "Онлайн");
+            builder.SetBudget(500, "14 дней");
+            builder.AddChannel("Email");
+            builder.SetSchedule("01.06.2026", "14.06.2026");
+            builder.SetTargetingOptions("По сегментам");
+            builder.DefineMetrics("Открытия");
+            builder.DefineMetrics("Клики");
+            return builder.Build();
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Campaign builder = new Campaign();
+            builder.SetAudience("25-35 лет", "Астана");
+            builder.SetBudget(1500, "7 дней");
+            builder.AddChannel("Instagram");
+            builder.AddChannel("Email");
+            builder.SetSchedule("10.06.2026", "17.06.2026");
+            builder.SetTargetingOptions("По интересам");
+            builder.DefineMetrics("Клики");
+            builder.DefineMetrics("Показы");
+            AdCampaign custom = builder.Build();
+            custom.ShowInfo();
+
+            AdCampaign social = SocialMediaCampaign.Create();
+            social.ShowInfo();
+
+            AdCampaign search = SearchEngineCampaign.Create();
+            search.ShowInfo();
+
+            AdCampaign email = EmailMarketingCampaign.Create();
+            email.ShowInfo();
+        }
+    }
 }
